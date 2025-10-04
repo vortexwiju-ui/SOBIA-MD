f855e8e28d82dac5e46622cc4f5ab0f53d4c33b1
vortexwiju-ui <vortexwiju@gmail.com>
2025-10-04 07:33:14 +0500

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: 'SOBIA MD DOWNLOAD📂DOG PICS\n\n\n> *BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: 'SOBIA MD DOWNLOAD📂DOG PICS\n\n\n> *BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`Error Fetching Dog Image🤕: ${e.message}`);
