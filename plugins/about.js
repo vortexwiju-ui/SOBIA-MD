e4eec618aec73f882fb112a3d79587ca7da9d158
vortexwiju-ui <vortexwiju@gmail.com>
2025-10-04 07:06:39 +0500

    Update about.js

--- plugins/about.js
+++ plugins/about.js
@@ -11,7 +11,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
 let madeMenu = `❁ ════ ❃•⇆•❃ ════ ❁
 
-*👋 HELLO ${pushname}*\n\n *I AM SOBIA BUTT 746*\n\n *I AM 20 YEARS OLD Girl 🔞*\n\n *I AM FROM PERSNOL HAI😁🙈*\n\n *I AM SIMPLE WHATSAPP BOT DEVELOPER👨‍💻*\n\n\n *THANK YOU.😊*
+*👋 HELLO ${pushname}*\n\n *I AM SOBIA ASSISTANT OF YOUR VORTEX*\n\n *I AM 17 YEARS OLD Girl 🔞*\n\n *I AM FROM PERSNOL HAI😁🙈*\n\n *I AM SIMPLE WHATSAPP BOT DEVELOPER👨‍💻*\n\n\n *THANK YOU.😊*
 
 ❁ ════ ❃•⇆•❃ ════ ❁
 
