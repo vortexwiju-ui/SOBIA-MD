04039dd70b39f462fb86214bfa7a97512e9e1a1e
vortexwiju-ui <vortexwiju@gmail.com>
2025-10-04 07:21:52 +0500

    Update animegirl.js

--- plugins/animegirl.js
+++ plugins/animegirl.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -34,7 +34,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SILENT-SOBX-MD BY SILENTLOVER432*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©VORTEX-SOBX-MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -74,7 +74,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -94,7 +94,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
@@ -114,7 +114,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY SOBIA BUTT*' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.url }, caption: '👸 *SOBIA-MD RANDOM ANIME GIRL IMAGES* 👸\n\n\n *🧬©SOBIA MD BY YOUR VORTEX*' }, { quoted: mek });
     } catch (e) {
         console.log(e);
         reply(`*Error Fetching Anime Girl image*: ${e.message}`);
